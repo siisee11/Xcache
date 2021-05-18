1baa07e857f2081df3d68287350cde740775478a
JY <siisee111@gmail.com>
2021-05-18 17:51:21 +0900

    Update fio

--- client/fio_test/run_cgroup_fio.sh
+++ client/fio_test/run_cgroup_fio.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
 
-fio $1 --status-interval=1 --output-format=terse --output=out &
+#fio $1 --status-interval=1 --output-format=terse --output=out &
+fio $1 --output=out &
 echo $! > /dev/cgroup/memory/test_process/tasks &
 echo "START CGROUP FIO"
 
-wait
+#wait
